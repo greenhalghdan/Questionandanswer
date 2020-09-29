5b41250a0a02d9d8cf15a92093b2ea4e9631f02b
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2020-09-29 13:14:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your First Name") ;
+var lastName = prompt("what is your Last Name");
+var age = prompt("What is your age?");
+alert("Your name is " + firstName + " " + lastName);
+console.log("Your name is " + firstName + " " + lastName);
+alert("Your age is " + age);
+console.log("your age is " + age);
